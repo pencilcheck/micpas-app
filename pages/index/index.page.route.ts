e1a1c20d7f9a3fcd62954103d01c0befc01f8320
Penn Su <pennsu@gmail.com>
2024-05-21 12:40:41 -0700

    Fix some login redirect issue

--- pages/index/index.page.route.ts
+++ pages/index/index.page.route.ts
@@ -3,6 +3,6 @@ import { render, redirect } from 'vike/abort'
 export const guard = (pageContext) => {
   const { user } = pageContext
   if (user) {
-    throw redirect('/tabs') // login automatically
+    throw redirect('/ywwu') // login automatically
   }
 }
